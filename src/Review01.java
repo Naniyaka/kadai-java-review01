22ce30cbeb86a957a054b2af06eaf290d25b4077
Naniyaka <nanaka124367@gmail.com>
2025-03-06 20:08:29 +0900

    小数切り捨て直し

--- src/Review01.java
+++ src/Review01.java
@@ -2,18 +2,18 @@
 public class Review01 {
 
     public static void main(String[] args) {
-    double a = 1500;
+    int a = 1500;
     double salesTax = (10.0/100.0);
     //System.out.println("消費税は" + salesTax);
-    double result;
+    int result;
     result = tax(a, salesTax);
-    double b = a + result;
-        System.out.println((int)a + "円の商品の税込価格は"+ (int)b + "円（消費税は" + (int)result + "円）です。");
+    int b = a + result;
+        System.out.println(a + "円の商品の税込価格は"+ b + "円（消費税は" + result + "円）です。");
     }
 
-    public static double tax(double a, double salesTax) {
-        double Tax = a * salesTax;
-        return Tax;
+    public static int tax(int a, double salesTax) {
+        double Tax = Math.floor(a * salesTax);
+        return (int)Tax;
     }
 }
 
